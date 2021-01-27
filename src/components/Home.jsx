9abccdcd2f29aaf5fe4b8b7f8bb48a87fc3a0c8e
Mumblinmad <linusbmartin@gmail.com>
2021-01-27 10:20:13 -0600

    UpToDate

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,8 +5,13 @@ class Home extends Component {
     render() { 
         return (  
             <div className={"home " + this.props.className}>
+                <div className="container">
+                <div className="glassBg"></div>
+                <div className='homeText'>
                 <h1>Linus Martin</h1>
                 <h4>Web Developer</h4>
+                </div>
+                </div>
             </div>
 
         );
---
+++ src/components/Info.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+class Info extends Component {
+
+    
+    render() { 
+        return ( 
+            <div className="display info">
+                <h1>Resume</h1>
+                {/* Education */}
+                {/* Skills */}
+                <ul>
+                    <li>One</li>
+                    <li>Two</li>
+                    <li>Three</li>
+                    <li>Four</li>
+                </ul>
+                {/* Technologies */}
+            </div>
+         );
+    }
+}
+ 
+export default Info;
